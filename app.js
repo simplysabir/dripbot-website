052a93a5a097367b8b84684f88e8463f7c3c90f1
simplysabir <simplysabir@gmail.com>
2024-02-27 17:26:31 +0530

    format

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
+$("#toggle").click(function () {
+  $(this).toggleClass("on");
+  $("#resize").toggleClass("active");
+});
 
- 
-      $("#toggle").click(function() {
-        $(this).toggleClass('on');
-        $("#resize").toggleClass("active");
-  });
+$("#resize ul li a").click(function () {
+  $(this).toggleClass("on");
+  $("#resize").toggleClass("active");
+});
 
-  $("#resize ul li a").click(function() {
-        $(this).toggleClass('on');
-        $("#resize").toggleClass("active");
-  });
+$(".close-btn").click(function () {
+  $(this).toggleClass("on");
+  $("#resize").toggleClass("active");
+});
 
-  $(".close-btn").click(function() {
-        $(this).toggleClass('on');
-        $("#resize").toggleClass("active");
-  });
+TweenMax.from("#brand", 1, {
+  delay: 0.4,
+  y: 10,
+  opacity: 0,
+  ease: Expo.easeInOut,
+});
 
-   
+TweenMax.staggerFrom(
+  "#menu li a",
+  1,
+  {
+    delay: 0.4,
+    opacity: 0,
+    ease: Expo.easeInOut,
+  },
+  0.1
+);
 
-  TweenMax.from("#brand", 1, {
-        delay: 0.4,
-        y: 10,
-        opacity: 0,
-        ease: Expo.easeInOut
-  })
-
-  TweenMax.staggerFrom("#menu li a", 1, {
-        delay: 0.4,
-        opacity: 0,
-        ease: Expo.easeInOut
-  }, 0.1);
-
- 
-
-  new WOW().init();
-
-  
+new WOW().init();
