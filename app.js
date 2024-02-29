67be460724e1f3a6aa86f709579a9997834efa24
0xNoushad <choudharynoushad46@gmail.com>
2024-02-29 17:41:43 +0530

    update

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
-$("#toggle").click(function () {
-  $(this).toggleClass("on");
-  $("#resize").toggleClass("active");
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('.wrapper'),  
+  smooth: true
 });
 
-$("#resize ul li a").click(function () {
-  $(this).toggleClass("on");
-  $("#resize").toggleClass("active");
+  $("#toggle").click(function() {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
 });
 
-$(".close-btn").click(function () {
-  $(this).toggleClass("on");
-  $("#resize").toggleClass("active");
+$("#resize ul li a").click(function() {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
 });
+
+$(".close-btn").click(function() {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+const hero = document.querySelector(".hero");
+const linksContainer = document.querySelector(".links");
+
+hero.addEventListener("animationend", function () {
+    hero.classList.add("show-links");
+});
+});
+
+
 
 TweenMax.from("#brand", 1, {
-  delay: 0.4,
-  y: 10,
-  opacity: 0,
-  ease: Expo.easeInOut,
-});
+    delay: 0.4,
+    y: 10,
+    opacity: 0,
+    ease: Expo.easeInOut
+})
 
-TweenMax.staggerFrom(
-  "#menu li a",
-  1,
-  {
+TweenMax.staggerFrom("#menu li a", 1, {
     delay: 0.4,
     opacity: 0,
-    ease: Expo.easeInOut,
-  },
-  0.1
-);
+    ease: Expo.easeInOut
+}, 0.1);
+
+
 
 new WOW().init();
+
